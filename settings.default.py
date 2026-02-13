664c279c2cf30ff1067b5cd279b964d191e84de3
sakshamjainwin <sakshjainn@gmail.com>
2026-02-13 21:47:04 +0000

    Initial commit

--- settings.default.py
+++
@@ -1,3 +0,0 @@
-datasets_dir = '/home/user/data/datasets'
-results_base_dir = '/home/user/data/projects_data/dataset_reconstruction/runs/'
-models_dir = '/home/user/data/projects_data/dataset_reconstruction/models'
